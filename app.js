4ea391ea0b5b1d382ef28769e410cb9f2d60632b
Himanshu Rawat <himanshurawat180@gmail.com>
2023-04-23 11:48:34 +0530

    Name changes

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const Victim = require("./models/Victim");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Victim = require('./models/Victim');
 
-require("dotenv/config");
+require('dotenv/config');
 
 //Import Routes
 
-const adminRoute = require("./routes/admins");
+const adminRoute = require('./routes/admins');
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/admin", adminRoute);
+app.use('/admin', adminRoute);
 
 //Find victim
-app.post("/findByAadhar", async (req, res) => {
-  try {
-    const victim = await Victim.findOne({ aadhar: req.body.aadhar });
-    res.status(200).send(victim);
-  } catch (err) {
-    res.status(401).send({
-      message: "Not Found",
-      error: err,
-    });
-  }
+app.post('/searchByAadhar', async (req, res) => {
+	try {
+		const victim = await Victim.findOne({ aadhar: req.body.aadhar });
+		res.status(200).send(victim);
+	} catch (err) {
+		res.status(401).send({
+			message: 'Not Found',
+			error: err,
+		});
+	}
 });
 
-app.post("/findByPhone", async (req, res) => {
-  try {
-    const victim = await Victim.findOne({ phone: req.body.phone });
-    res.status(200).send(victim);
-  } catch (err) {
-    res.status(401).send({
-      message: "Not Found",
-      error: err,
-    });
-  }
+app.post('/searchByPhone', async (req, res) => {
+	try {
+		const victim = await Victim.findOne({ phone: req.body.phone });
+		res.status(200).send(victim);
+	} catch (err) {
+		res.status(401).send({
+			message: 'Not Found',
+			error: err,
+		});
+	}
 });
 
 mongoose.connect(process.env.MONGO_URI, () =>
-  console.log("~~~CONNECTED TO DB~~~")
+	console.log('~~~CONNECTED TO DB~~~')
 );
 
 const listener = app.listen(5000);
-console.log("Express server listening on port %d", listener.address().port);
+console.log('Express server listening on port %d', listener.address().port);
